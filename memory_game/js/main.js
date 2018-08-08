2f0530e913516e41d5eebfbdf4a5460f610d0a24
claraonmars <mac@Furse.local>
2018-08-09 01:42:21 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,6 +30,7 @@ var cards = [{
 }
 ];
 var cardsInPlay =[];
+var score = 0;
 
 var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1]){
@@ -40,16 +41,58 @@ var checkForMatch = function(){
   }
 };
 
-var flipCard = function(cardId){
+var flipCard = function(){
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
+
+  this.setAttribute('src', cards[cardId].cardImage);
 
   if (cardsInPlay.length === 2){
   checkForMatch();
 }
 };
 
-flipCard(0);
-flipCard(2);
+// flipCard(0);
+// flipCard(2);
+
+var reset = function(){
+  for ( var i = 0; i < cards.length; i++){
+  var cardElement = document.getElementsByTagName('img')[i];
+
+  cardElement.setAttribute('src','images/back.png');
+  cardsInPlay = [];
+  }
+};
+
+var createBoard = function(){
+  for ( var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src','images/back.png');
+    cardElement.setAttribute('data-id',i);
+
+    cardElement.addEventListener('click',flipCard);
+    cardElement.addEventListener('click',getScore);
+
+    document.getElementById('game-board').appendChild(cardElement);
+
+  }
+};
+
+
+var getScore = function(){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    score += 1;
+  }
+  else{
+  }
+
+  document.getElementById('score').innerHTML = "Your Score: " + score;
+};
+
+createBoard();
+
+var button = document.getElementsByTagName('button')[0];
+button.addEventListener('click',reset);
